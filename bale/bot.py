705832b8ad52040f5c6853958b16bc426be58cd1
mahakteam <88476945+kianahmadian@users.noreply.github.com>
2022-07-25 14:17:40 +0430

    Open Commit to get information

--- bale/bot.py
+++ bale/bot.py
@@ -3,27 +3,56 @@ import asyncio
 from bale import (Message, Update, User, Components, Chat, Price, ChatMember, HTTPClient)
 
 
+__all__ = (
+    "Bot"
+)
+
+
+class _Loop:
+    __slots__ = ()
+    
+    
+_loop = _Loop()
+
+
 class Bot:
     """This object represents a Bale Bot.
 
         Args:
             token (str): Bot Token.
-
+                
         Raises:
             :class:`bale.Error`
     """
     __slots__ = (
+        "loop",
         "token",
         "loop",
         "_user",
-        "http"
+        "http",
+        "_closed"
     )
 
     def __init__(self, token: str, **kwargs):
+        self.loop = _loop
         self.token = token
-        self.loop = kwargs.get("loop") if kwargs.get("loop") is not None else asyncio.get_event_loop()
-        self.http = HTTPClient(loop=self.loop, connector=kwargs.get("connector"), token=token)
-        self._user = self.http.get_bot()
+        self.http: HTTPClient = HTTPClient(loop=self.loop, connector=kwargs.get("connector"), token=token)
+        self._user = None
+        self._closed = False
+
+    async def __aenter__(self):
+        loop = asyncio.get_running_loop()
+        self.loop = loop
+        self.http.loop = loop
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        if not self.is_closed():
+            self._closed = True
+
+    def is_closed(self):
+        """:class:`bool`: Connection Status"""
+        return self._closed
 
     def _get_bot(self) -> User:
         """Get Bot.
@@ -49,16 +78,16 @@ class Bot:
             self._user = self._get_bot()
         return self._user
 
-    def delete_webhook(self) -> bool:
+    async def delete_webhook(self) -> bool:
         """This service is used to remove the web hook set for the arm.
 
         Returns:
             bool: If done "True" If not "False"
         """
-        response, payload = self.http.delete_webhook()
+        response, payload = await self.http.delete_webhook()
         return payload["result"]
 
-    def send_message(self, chat_id: str, text: str = None, components=None, reply_to_message_id: str = None) -> Message | None:
+    async def send_message(self, chat_id: str, text: str = None, components=None, reply_to_message_id: str = None) -> Message | None:
         """This service is used to send text messages.
         
         Args:
@@ -71,7 +100,6 @@ class Bot:
         Returns:
             :class:`bale.Message`: On success, the sent Message is returned.
         """
-
         if components:
             if isinstance(components, Components):
                 components = components.to_dict()
@@ -79,13 +107,10 @@ class Bot:
                 components = components
         if reply_to_message_id:
             reply_to_message_id = reply_to_message_id
-        response, payload = self.http.send_message(chat_id, text, components=components, reply_to_message_id=reply_to_message_id)
+        response, payload = await self.http.send_message(chat_id, text, components=components, reply_to_message_id=reply_to_message_id)
         return Message.from_dict(data=payload["result"], bot=self)
 
-    def send_invoice(self, chat_id: str, title: str, description: str, provider_token: str, prices: Price,
-                     photo_url: str = None, need_name: bool = False,
-                     need_phone_number: bool = False, need_email: bool = False, need_shipping_address: bool = False,
-                     is_flexible: bool = True) -> Message | None:
+    async def send_invoice(self, chat_id: str, title: str, description: str, provider_token: str, prices: Price, photo_url: str = None, need_name: bool = False, need_phone_number: bool = False, need_email: bool = False, need_shipping_address: bool = False, is_flexible: bool = True) -> Message | None:
         """You can use this service to send money request messages.
         Args:
             chat_id (str): Chat ID
@@ -113,10 +138,10 @@ class Bot:
                 else:
                     key_list.append(i)
             prices = key_list
-        response, payload = self.http.send_invoice(chat_id, title, description, provider_token, prices, photo_url, need_name, need_phone_number, need_email, need_shipping_address, is_flexible)
+        response, payload = await self.http.send_invoice(chat_id, title, description, provider_token, prices, photo_url, need_name, need_phone_number, need_email, need_shipping_address, is_flexible)
         return Message.from_dict(data=payload["result"], bot=self)
 
-    def edit_message(self, chat_id: str, message_id: str, text: str, components=None) -> Message:
+    async def edit_message(self, chat_id: str, message_id: str, text: str, components=None) -> Message:
         """You can use this service to edit text messages that you have already sent through the arm.
         
         Args:
@@ -132,10 +157,10 @@ class Bot:
         if components:
             components = components.to_dict() if isinstance(components, Components) else components
 
-        response, payload = self.http.edit_message_text(chat_id, message_id, text, components)
+        response, payload = await self.http.edit_message_text(chat_id, message_id, text, components)
         return payload
 
-    def delete_message(self, chat_id: str, message_id: str) -> bool:
+    async def delete_message(self, chat_id: str, message_id: str) -> bool:
         """You can use this service to delete a message that you have already sent through the arm.
         
         In Channel or Group:
@@ -150,10 +175,10 @@ class Bot:
         Return:
             bool: if done "True" if not "False"
         """
-        response, payload = self.http.delete_message(chat_id, message_id)
+        response, payload = await self.http.delete_message(chat_id, message_id)
         return payload["result"]
 
-    def get_chat(self, chat_id: str) -> Chat:
+    async def get_chat(self, chat_id: str) -> Chat:
         """This service can be used to receive personal information that has previously interacted with the arm.
 
         Args:
@@ -163,10 +188,10 @@ class Bot:
         Return:
             :class:`bale.Chat`: On success, the sent Message is returned.
         """
-        response, payload = self.http.get_chat(chat_id)
+        response, payload = await self.http.get_chat(chat_id)
         return Chat.from_dict(payload["result"], bot=self)
 
-    def get_chat_member(self, chat_id: str, user_id: str) -> "ChatMember":
+    async def get_chat_member(self, chat_id: str, user_id: str) -> "ChatMember":
         """
             Args:
                 chat_id (str): Group ID
@@ -176,10 +201,10 @@ class Bot:
             Raises:
                 :class:`bale.Error`
         """
-        response, payload = self.http.get_chat_member(chat_id=chat_id, member_id=user_id)
+        response, payload = await self.http.get_chat_member(chat_id=chat_id, member_id=user_id)
         return ChatMember.from_dict(payload["result"])
 
-    def get_chat_members_count(self, chat_id: str) -> int:
+    async def get_chat_members_count(self, chat_id: str) -> int:
         """
             Args:
                 chat_id (str): Group ID
@@ -188,10 +213,10 @@ class Bot:
             Raises:
                 :class:`bale.Error`
         """
-        response, payload = self.http.get_chat_members_count(chat_id)
+        response, payload = await self.http.get_chat_members_count(chat_id)
         return payload["result"]
 
-    def get_chat_administrators(self, chat_id: str) -> list["ChatMember"]:
+    async def get_chat_administrators(self, chat_id: str) -> list["ChatMember"]:
         """This service can be used to display admins of a group or channel.
 
         Args:
@@ -201,14 +226,14 @@ class Bot:
         Returns:
             List[:class:`bale.ChatMember`]
         """
-        response, payload = self.http.get_chat_administrators(chat_id)
+        response, payload = await self.http.get_chat_administrators(chat_id)
         members = []
         for member_payload in payload["result"]:
             members.append(ChatMember.from_dict(data=member_payload))
         return members
 
-    def get_updates(self, offset: int = None, limit: int = None) -> list["Update"] | None:
-        """Use this method to receive incoming updates using long polling. 
+    async def get_updates(self, offset: int = None, limit: int = None) -> list["Update"] | None:
+        """Use this method to receive incoming updates using long polling.
 
         Args:
             offset (int, optional): Defaults to None.
@@ -219,7 +244,7 @@ class Bot:
         Returns:
             List[:class:`bale.Update`]
         """
-        response, payload = self.http.get_updates(offset, limit)
+        response, payload = await self.http.get_updates(offset, limit)
         updates = []
         for update_payload in payload.get("result"):
             if offset is not None and update_payload.get("update_id") < offset:
@@ -227,3 +252,10 @@ class Bot:
             update = Update.from_dict(data=update_payload, bot=self)
             updates.append(update)
         return updates
+
+    async def run(self):
+        async def main():
+            async with self:
+                self._user = self._get_bot()
+
+        asyncio.run(main())
