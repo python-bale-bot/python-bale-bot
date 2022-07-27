955905202a20bcae44cdb10bb9ad2028312dc49b
mahakteam <88476945+kianahmadian@users.noreply.github.com>
2022-07-27 17:18:52 +0430

    Add `close` function to be Httpclient class. Improve the code.

--- bale/request/http.py
+++ bale/request/http.py
@@ -1,20 +1,37 @@
+"""
+    MIT License
+
+    Copyright (c) 2022 kian Ahmadian
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+"""
+
 from bale.version import BALE_API_BASE_URL
 import aiohttp
 import asyncio
 from ..error import (NetworkError, HTTPException, TimeOut, NotFound, Forbidden, APIError)
 
-__all__ = (
-	"HTTPClient"
-)
+__all__ = ("HTTPClient",)
 
 
 class Route:
-	"""
-		Args:
-			method (str): Request Method
-			path (str): Request Path
-			token (str): Bot Token
-	"""
+	"""Route Class for http"""
 	BASE = BALE_API_BASE_URL
 
 	__slots__ = (
@@ -57,6 +74,10 @@ class HTTPClient:
 		if self.__session.closed:
 			self.__session = aiohttp.ClientSession(connector=self.connector)
 
+	async def close(self):
+		if self.__session:
+			await self.__session.close()
+
 	async def request(self, route: Route, **kwargs):
 		url = route.url
 		method = route.method
@@ -90,8 +111,6 @@ class HTTPClient:
 			raise NetworkError(error)
 		except aiohttp.client_exceptions.ServerTimeoutError:
 			raise TimeOut()
-
-		# raise HTTPException(response, response.json())
 
 	def send_message(self, chat_id, text, *, components=None, reply_to_message_id=None):
 		payload = {
