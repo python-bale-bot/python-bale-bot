ddd5e25957318bd78f54ecc359039328d0c678b2
mahakteam <88476945+kianahmadian@users.noreply.github.com>
2022-09-14 20:05:37 +0430

    Add `Photo` to __all__

--- bale/attachments/__init__.py
+++ bale/attachments/__init__.py
@@ -2,11 +2,12 @@ from .document import Document
 from .audio import Audio
 from .contact import ContactMessage
 from .location import Location
-
+from .photo import Photo
 
 __all__ = (
 	"Document",
 	"Audio",
 	"ContactMessage",
-	"Location"
+	"Location",
+	"Photo"
 )
