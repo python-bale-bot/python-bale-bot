515b71a46885b2afa42a6234d7f0a9b6a36dd156
mahakteam <88476945+kianahmadian@users.noreply.github.com>
2022-09-23 01:45:56 +0330

    Create `start` function Fix `reload_session` bugs

--- bale/request/http.py
+++ bale/request/http.py
@@ -66,8 +66,12 @@ class HTTPClient:
 
 	def reload_session(self):
 		"""Reset Session"""
-		if self.__session.closed:
+		if self.__session and self.__session.closed:
 			self.__session = aiohttp.ClientSession(loop=self.loop, connector=aiohttp.TCPConnector(limit=0))
+
+	async def start(self):
+		"""Start Http client"""
+		self.__session = aiohttp.ClientSession(loop=self.loop, connector=aiohttp.TCPConnector(limit=0))
 
 	async def close(self):
 		"""Close Session connection"""
@@ -155,7 +159,6 @@ class HTTPClient:
 		return self.request(Route("GET", "deleteWebhook", self.token))
 
 	def get_bot(self):
-		self.__session = aiohttp.ClientSession(loop=self.loop, connector=aiohttp.TCPConnector(limit=0))
 		return self.request(Route("GET", "getme", self.token))
 
 	def get_chat(self, chat_id):
