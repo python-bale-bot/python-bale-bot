168346f487523a8f550674c3c4a42097ac340eea
kian ahmadian <k.ahmadian87@gmail.com>
2022-10-25 20:29:51 +0330

    Create `ResponseParser` class for requests

---
+++ bale/request/parser.py
@@ -0,0 +1,49 @@
+from aiohttp import ClientResponse
+from typing import Any
+
+class ResponseParser:
+	"""a Parser for parse http response.
+
+	Attributes
+	==========
+	ok: bool
+		Status of the response
+
+	result: Any
+		Result of the response
+
+	error_code: Optional[:class:`int`]
+		Error Code.
+		``None`` when no error
+
+	description: Optional[:class:`str`]
+		Description of the error.
+		``None`` when no error.
+
+	_raw: dict
+		Raw of the response data
+	"""
+
+	__slots__ = (
+		"result",
+		"error_code",
+		"description",
+		"ok",
+		"_raw"
+	)
+
+	def __init__(self, ok: bool, result: Any = None, error_code: int = None, description: str = None, raw: dict = None):
+		self.ok = ok
+
+		self.result = result
+		self.error_code = error_code
+		self.description = description
+		self._raw = raw
+
+	@classmethod
+	async def from_response(cls, data: "ClientResponse"):
+		if data.status == 404:
+			return cls(False, raw=dict())
+		data = await data.json()
+		return cls(data.get("ok", False), data.get("result"), data.get("error_code"), data.get("description"), data)
+
