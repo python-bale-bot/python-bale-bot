253d0fe13b3817b545d070826622a7cf4c7d17ef
kian ahmadian <dev.kian.ahmadian@gmail.com>
2023-03-23 14:38:52 +0430

    Update version to be `2.4.4`

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ extras_require = {
 if __name__ == "__main__":
     setup(
         name="python-bale-bot",
-        version="2.4.3",
+        version="2.4.4",
         platforms=["Windows", ],
         fullname="python-bale-bot",
         description="An API wrapper for Bale written in Python",
