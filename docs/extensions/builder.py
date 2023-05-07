74b08c4aab7ea3ed2dc0b2c935dfd456fcf93d3b
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-05-07 20:00:53 +0430

    docs: Fix `ImportError` Error

--- docs/extensions/builder.py
+++ docs/extensions/builder.py
@@ -1,7 +1,7 @@
 from sphinx.builders.html import StandaloneHTMLBuilder
 from sphinx.builders.gettext import MessageCatalogBuilder, I18nBuilder, timestamp, ltz, should_write, GettextRenderer
 from sphinx.locale import __
-from sphinx.util import status_iterator
+from sphinx.util.display import status_iterator
 from sphinx.util.osutil import ensuredir
 from sphinx.environment.adapters.indexentries import IndexEntries
 from sphinx.writers.html5 import HTML5Translator
@@ -29,10 +29,7 @@ class DPYHTML5Translator(HTML5Translator):
 
 
 class DPYStandaloneHTMLBuilder(StandaloneHTMLBuilder):
-    # This is mostly copy pasted from Sphinx.
     def write_genindex(self) -> None:
-        # the total count of lines for each index letter, used to distribute
-        # the entries into two columns
         genindex = IndexEntries(self.env).create_index(self, group_entries=False)
         indexcounts = []
         for _k, entries in genindex:
