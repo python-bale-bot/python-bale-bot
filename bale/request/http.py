360931fabdcaf18ae8f2fd7e11c42aab39108fd3
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-05-17 22:38:40 +0330

    Fix `AttributeError` bug in rate limit

--- bale/request/http.py
+++ bale/request/http.py
@@ -56,8 +56,7 @@ class HTTPClient:
 	__slots__ = (
 		"_loop",
 		"token",
-		"__session",
-		"rate_limit"
+		"__session"
 	)
 
 	def __init__(self, loop, token):
@@ -75,7 +74,6 @@ class HTTPClient:
 	@loop.setter
 	def loop(self, _value):
 		self._loop = _value
-		self.rate_limit._loop = _value
 
 	def reload_session(self):
 		"""Reset Session"""
@@ -97,41 +95,40 @@ class HTTPClient:
 	async def request(self, route: Route, **kwargs):
 		url = route.url
 		method = route.method
-		async with self.rate_limit:
-			for tries in range(5):
-				try:
-					async with self.__session.request(method=method, url=url, **kwargs) as response:
-						response: aiohttp.ClientResponse = response
-						parsed_response = await ResponseParser.from_response(response)
-						if response.status == ResponseStatusCode.OK:
-							return parsed_response
-						elif response.status == ResponseStatusCode.NOT_FOUND:
-							raise NotFound(parsed_response.description)
-						elif response.status == ResponseStatusCode.PERMISSION_DENIED:
+		for tries in range(5):
+			try:
+				async with self.__session.request(method=method, url=url, **kwargs) as response:
+					response: aiohttp.ClientResponse = response
+					parsed_response = await ResponseParser.from_response(response)
+					if response.status == ResponseStatusCode.OK:
+						return parsed_response
+					elif response.status == ResponseStatusCode.NOT_FOUND:
+						raise NotFound(parsed_response.description)
+					elif response.status == ResponseStatusCode.PERMISSION_DENIED:
+						raise Forbidden()
+					elif not parsed_response.ok or response.status in (ResponseStatusCode.NOT_INCORRECT, ResponseStatusCode.RATE_LIMIT):
+						if parsed_response.description == HTTPClientError.USER_OR_CHAT_NOT_FOUND:
+							raise NotFound("User or Chat not Found")
+						elif response.status == ResponseStatusCode.RATE_LIMIT or parsed_response.description in (HTTPClientError.RATE_LIMIT, HTTPClientError.LOCAL_RATE_LIMIT):
+							if tries >= 4:
+								raise RateLimited()
+
+							await asyncio.sleep((1 + tries) * 2)
+							continue
+						elif parsed_response.description == HTTPClientError.PERMISSION_DENIED:
 							raise Forbidden()
-						elif not parsed_response.ok or response.status in (ResponseStatusCode.NOT_INCORRECT, ResponseStatusCode.RATE_LIMIT):
-							if parsed_response.description == HTTPClientError.USER_OR_CHAT_NOT_FOUND:
-								raise NotFound("User or Chat not Found")
-							elif response.status == ResponseStatusCode.RATE_LIMIT or parsed_response.description in (HTTPClientError.RATE_LIMIT, HTTPClientError.LOCAL_RATE_LIMIT):
-								if tries >= 4:
-									raise RateLimited()
 
-								await asyncio.sleep((1 + tries) * 2)
-								continue
-							elif parsed_response.description == HTTPClientError.PERMISSION_DENIED:
-								raise Forbidden()
-
-							raise APIError(
-									str(parsed_response.error_code), parsed_response.description
-								)
-				except aiohttp.ClientConnectorError as error:
-					raise NetworkError(str(error))
-				except aiohttp.ServerTimeoutError:
-					raise TimeOut()
-				except aiohttp.ClientOSError as error:
-					raise BaleError(str(error))
-				except Exception as error:
-					raise HTTPException(error)
+						raise APIError(
+								str(parsed_response.error_code), parsed_response.description
+							)
+			except aiohttp.ClientConnectorError as error:
+				raise NetworkError(str(error))
+			except aiohttp.ServerTimeoutError:
+				raise TimeOut()
+			except aiohttp.ClientOSError as error:
+				raise BaleError(str(error))
+			except Exception as error:
+				raise HTTPException(error)
 
 	async def get_file(self, file_id):
 		async with self.__session.get("{base_file_url}/bot{token}/{file_id}".format(base_file_url = BALE_API_FILE_URL, token = self.token, file_id = file_id)) as response:
