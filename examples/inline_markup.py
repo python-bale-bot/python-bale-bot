6dd7658aa5f0c58d8e80481daf012406f160a1b6
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-06-25 12:54:19 -0700

    Update examples directory

--- examples/inline_markup.py
+++ examples/inline_markup.py
@@ -9,18 +9,20 @@ async def when_bot_is_ready():
 @client.listen(EventType.MESSAGE)
 async def when_receive_message(message: Message):
 	if message.content == "/start":
+		component = Components()
+		component.add_inline_keyboard(InlineKeyboard(text="what is python-bale-bot?", callback_data="python-bale-bot:help"))
+		component.add_inline_keyboard(InlineKeyboard(text="package site", url="https://python-bale-bot.ir"), row=2)
+		component.add_inline_keyboard(InlineKeyboard(text="package GitHub", url="https://python-bale-bot.ir/github"), row=2)
 		await message.reply(
-			"Hey!",
-			components=Components(inline_keyboards=[[
-					InlineKeyboard("Send Hi", callback_data="send_hi")
-				]])
+			f"*Hi {message.author.first_name}, Welcome to python-bale-bot bot*",
+			components=component
 		)
 
 @client.listen(EventType.CALLBACK)
 async def when_receive_callback(callback: CallbackQuery):
-	if callback.data == "send_hi":
+	if callback.data == "python-bale-bot:help":
 		await callback.message.reply(
-			f"Hi {callback.user.first_name}"
+			"*python-bale-bot* is a Python library for building bots on the Bale messenger platform. Bale is a messaging app that provides a secure and private messaging experience for users. The python-bale-bot library provides a simple and easy-to-use interface for building bots on the Bale platform, allowing developers to create bots that can send and receive messages, handle events, and perform various actions on behalf of users."
 		)
 
 client.run()
