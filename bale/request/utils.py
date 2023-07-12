ffe8ac6bdb9415ef7cda2029d9e9fad27742b01c
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-07-12 21:04:19 +0330

    Create function for convert to json

--- bale/request/utils.py
+++ bale/request/utils.py
@@ -1,6 +1,12 @@
+from typing import Any
+import json
+
 class ResponseStatusCode:
 	OK = 200
 	NOT_INCORRECT = 400
 	NOT_FOUND = 440
 	PERMISSION_DENIED = 403
 	RATE_LIMIT = 429
+
+def to_json(obj: Any) -> str:
+	return json.dumps(obj)
