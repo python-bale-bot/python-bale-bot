cfcf7ddb69ecf1aaac45d7559110a3fb1ef00708
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2023-12-27 15:16:14 +0330

    Use `BaleObject` object & Improve Code Quality

--- bale/callbackquery.py
+++ bale/callbackquery.py
@@ -22,8 +22,9 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 from __future__ import annotations
-from bale import User, Message
-from typing import TYPE_CHECKING
+from bale import BaleObject, User, Message
+from typing import Optional, Dict, TYPE_CHECKING
+
 if TYPE_CHECKING:
     from bale import Bot
 
@@ -32,40 +33,44 @@ __all__ = (
 )
 
 
-class CallbackQuery:
+class CallbackQuery(BaleObject):
     """
     This object represents an incoming callback query from a callback button in an inline keyboard.
 
     Attributes
     ----------
-        callback_id: :class:`str`
+        id: :class:`str`
             Unique identifier for this query.
         from_user: :class:`bale.User`
             Sender.
-        message: :class:`bale.Message`
+        message: Optional[:class:`bale.Message`]
             Message with the callback button that originated the query. Note that message content and message date will not be available if the message is too old.
-        inline_message_id: :class:`str`
+        inline_message_id: Optional[:class:`str`]
             Identifier of the message sent via the bot in inline mode, that originated the query.
-        data: :class:`str`
+        data: Optional[:class:`str`]
              Data associated with the callback button. Be aware that the message, which originated the query, can contain no callback buttons with this data.
     """
     __slots__ = (
-        "callback_id",
+        "id",
         "from_user",
         "message",
         "inline_message_id",
-        "data",
-        "bot"
+        "data"
     )
 
-    def __init__(self, callback_id: int, data: str = None, message: "Message" = None,
-                 inline_message_id: str = None, from_user: "User" = None, bot: "Bot" = None):
-        self.callback_id = callback_id
+    def __init__(
+            self, id: str, from_user: "User",
+            data: Optional[str], message: Optional["Message"],
+            inline_message_id: Optional[str]
+    ):
+        super().__init__()
+        self._id = id
+        self.id = id
+        self.from_user = from_user
+
         self.data = data
         self.message = message
         self.inline_message_id = inline_message_id
-        self.from_user = from_user
-        self.bot = bot
 
     @property
     def user(self):
@@ -73,16 +78,12 @@ class CallbackQuery:
         return self.from_user
 
     @classmethod
-    def from_dict(cls, data: dict, bot: "Bot"):
-        return cls(bot=bot, data=data.get("data"), callback_id=data.get("id"), message=Message.from_dict(data.get("message"), bot=bot),
-                   inline_message_id=data.get("inline_message_id"),
-                   from_user=User.from_dict(bot=bot, data=data.get("from")))
+    def from_dict(cls, data: Optional[Dict], bot: "Bot"):
+        data = BaleObject.parse_data(data)
+        if not data:
+            return None
 
-    def __eq__(self, other):
-        return isinstance(other, CallbackQuery) and self.callback_id == other.callback_id
+        data['from_user'] = User.from_dict(data.pop('from', None), bot)
+        data['message'] = Message.from_dict(data.get('message'), bot)
 
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __repr__(self):
-        return f"<CallbackQuery inline_message_id={self.inline_message_id} message={self.message} from_user={self.from_user} data={self.data}>"
+        return super().from_dict(data, bot)
