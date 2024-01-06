e5163e9580a31fb344bf97ba2678c19f8fd70a95
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2024-01-07 00:14:30 +0330

    Update basic.py example

--- examples/basic.py
+++ examples/basic.py
@@ -4,14 +4,19 @@ client = Bot(token="Your Token")
 
 @client.event
 async def on_ready():
-	print(client.user, "is Ready!")
+	print(client.user.username, "is Ready!")
 
 @client.event
 async def on_update(update: Update):
-	print(update.update_id, update.type)
+	print(update.update_id)
 
 @client.event
 async def on_message(message: Message):
-	await message.reply(text="Hi!")
+	if message.content == '/start': # to get caption or text of message
+		await message.reply('Hi, from python-bale-bot to everyone!')
+		if message.chat.is_group_chat:
+			await message.reply("It's is a special Hi for groups!") # work when message sent in a group
+
+# See https://docs.python-bale-bot.ir/en/stable/event.html to get more information about events!
 
 client.run()
