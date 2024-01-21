03b1b5064279a2f0a25400c035d6b40b9bb257a0
Kian Ahmadian <dev.kian.ahmadian@gmail.com>
2024-01-21 23:56:38 +0330

    Support `bale.PhotoSize` from `BaseFile`

--- bale/attachments/photosize.py
+++ bale/attachments/photosize.py
@@ -9,14 +9,14 @@
 # You should have received a copy of the GNU General Public License v2.0
 # along with this program. If not, see <https://www.gnu.org/licenses/gpl-2.0.html>.
 from __future__ import annotations
-from bale import BaleObject
+from .file import BaseFile
 from typing import Optional
 
 __all__ = (
     "PhotoSize",
 )
 
-class PhotoSize(BaleObject):
+class PhotoSize(BaseFile):
     """This object represents one size of a photo or a file/sticker thumbnail.
 
     Attributes
@@ -40,7 +40,7 @@ class PhotoSize(BaleObject):
         "file_size"
     )
     def __init__(self, file_id: str, file_unique_id: str, width: int, height: int, file_size: Optional[int]):
-        super().__init__()
+        super().__init__(file_id, file_unique_id, file_size, width=width, height=height)
         self._id = file_id
         self.file_id = file_id
         self.file_unique_id = file_unique_id
