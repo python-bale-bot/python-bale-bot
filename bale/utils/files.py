60f5ff4183e8d395dbd1129f04eca7895a6452a3
Kian Ahmadian <info@kian-ahmadian.ir>
2024-03-16 01:49:07 +0330

    Add new utils section `files`

---
+++ bale/utils/files.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from typing import Union, Any, Optional, Type, TYPE_CHECKING
+if TYPE_CHECKING:
+    from bale import BaleObject
+
+def is_file_valid(obj: Optional[Union[Path, str]]) -> Optional[Path]:
+    if obj is None:
+        return None
+
+    path = Path(obj)
+    return path if path.is_file() else None
+
+def parse_file_input(
+        file_input: Any,
+        attachment_type: Optional[Type["BaleObject"]] = None,
+        file_name: Optional[str] = None
+):
+    from bale import InputFile
+
+    if isinstance(file_input, bytes):
+        return InputFile(file_input, file_name=file_name)
+    elif isinstance(file_input, (Path, str)):
+        if result_path := is_file_valid(file_input):
+            return result_path.open(mode="rb")
+    elif attachment_type and isinstance(file_input, attachment_type):
+        return file_input.file_id # type: ignore
+
+    return file_input
