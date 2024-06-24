5ac3cd00b1e2ef1fb04451fa5f55b435cf86b742
M.A.H.A.K Team <108016898+codaliza@users.noreply.github.com>
2024-06-24 14:03:45 +0330

    Add type hint

--- bale/version.py
+++ bale/version.py
@@ -1,4 +1,6 @@
-__version__ = '2.5.0'
-BALE_API_BASE_URL = 'https://tapi.bale.ai/bot'
-BALE_API_FILE_URL = 'https://tapi.bale.ai/file/bot'
+from typing import Final
+
+__version__: Final = '2.5.0'
+BALE_API_BASE_URL: Final = 'https://tapi.bale.ai/bot'
+BALE_API_FILE_URL: Final = 'https://tapi.bale.ai/file/bot'
 BALE_API_VERSION = '2.0.0'
