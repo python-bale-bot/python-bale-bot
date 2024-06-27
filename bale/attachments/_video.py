1bba75e9973f9e189480f8d3f7f9fd24a8a8a510
M.A.H.A.K Team <108016898+codaliza@users.noreply.github.com>
2024-06-27 03:48:37 +0330

    Update _video.py

--- bale/attachments/_video.py
+++ bale/attachments/_video.py
@@ -24,8 +24,8 @@ class Video(BaseFile):
         file_id: :obj:`str`
             Identifier for this file, which can be used to download or reuse the file.
         file_unique_id: :obj:`str`
-            Unique identifier for this file, which is supposed to be the same over time and for different bots.
-            Can’t be used to download or reuse the file.
+            Unique identifier for this file, which is supposed to be the same over time and for 
+            different bots. Can’t be used to download or reuse the file.
         width: int
             Video width as defined by sender.
         height: str
